c3df66988239147af7b5e17885ca2cc3506f6089
Ben Parr <benparr64@outlook.com>
2024-01-22 22:46:10 +0000

    init

---
+++ src/components/Submitted.tsx
@@ -0,0 +1,10 @@
+export const Submitted = () => {
+  return (
+    <div>
+      <h2>Form Submitted Successfully!</h2>
+      <p>
+        Thank you for submitting the form. We have received your information.
+      </p>
+    </div>
+  );
+};
---
+++ src/components/TenancyForm.tsx
@@ -0,0 +1,97 @@
+import { Typography, TextField, Button } from "@mui/material";
+import { Formik, Form, Field } from "formik";
+
+interface FormData {
+  fullName: string;
+  firstName: string;
+  email: string;
+  moveInDate: string;
+}
+
+export interface TenancyFormProps {
+  setSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const TenancyForm = ({ setSubmitted }: TenancyFormProps) => {
+  const initialValues: FormData = {
+    fullName: "",
+    firstName: "",
+    email: "",
+    moveInDate: "",
+  };
+
+  const mockSubmit = (values: FormData) => {
+    console.log("Form submitted with values:", values);
+    setSubmitted(true);
+  };
+
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        Tenant Information Form
+      </Typography>
+
+      <Formik initialValues={initialValues} onSubmit={mockSubmit}>
+        <Form>
+          <div className="grid grid-cols-1 gap-4 mb-4">
+            <Field
+              as={TextField}
+              fullWidth
+              label="Full Name"
+              name="fullName"
+              required
+            />
+
+            <Field
+              as={TextField}
+              fullWidth
+              label="First Name"
+              name="firstName"
+              required
+            />
+
+            <Field
+              as={TextField}
+              fullWidth
+              label="Email"
+              name="email"
+              type="email"
+              required
+            />
+
+            <Field
+              as={TextField}
+              fullWidth
+              label="Move-in Date"
+              name="moveInDate"
+              type="date"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className="w-full"
+            >
+              Submit
+            </Button>
+            <Button
+              type="reset"
+              variant="outlined"
+              color="primary"
+              className="w-full"
+            >
+              Clear
+            </Button>
+          </div>
+        </Form>
+      </Formik>
+    </>
+  );
+};
